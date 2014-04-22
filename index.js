74d930a62bccfda45c8919b40b14d85183712dd4
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-04-21 18:24:11 -0700

    add check for nsenter. working version

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
-var pty = require('pty.js');
-
-var term = pty.spawn('bash', [], {
-    name: 'xterm-color',
+// Create terminal stream for a container using nsenter
+/* example usage
+krain(function() {
+  var inst = connect('26073',"--mount --uts --ipc --net --pid",
+    {
       cols: 80,
-        rows: 30,
-          cwd: process.env.HOME,
-            env: process.env
+      rows: 30
+    });
+  inst.pipe(process.stdout);
+  process.stdin.pipe(inst);
 });
+*/
 
-term.on('data', function(data) {
-    console.log(data);
-});
+var which = require('which');
+var pty = require('pty.js');
+var nsenter = "";
 
-term.write('ls\r');
-term.resize(100, 40);
-term.write('ls /\r');
-console.log(term.process);
+
+var krain = function(Opts, Cb) {
+  var opts = Opts;
+  var cb = Cb;
+  if (typeof opts == 'function') {
+    cb = Opts;
+    opts = {};
+  }
+  which('nsenter', function(err, cmdpath) {
+    if (err) {
+      cb(new Error('nsenter not avalible. module useless'));
+      return;
+    }
+    nsenter = cmdpath;
+    cb();
+  });
+};
+/*
+  pid of container to connect to *REQUIRED
+  Args are argument for nsenter. defaults to all
+  Opts are option for pty default empty
+  returns stream to STDIN of container
+*/
+var connect = function(pid, Args, Opts) {
+  var args = Args || "--mount --uts --ipc --net --pid";
+  var term = pty.spawn('bash',
+    ["-c", "sudo "+nsenter+" --target " + pid + " " + args],
+    Opts
+   );
+  return term;
+};
+
+
