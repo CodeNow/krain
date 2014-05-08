309471fb4f44f0e39087052cac036ecb7db9f95e
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-05-07 20:29:48 -0700

    removeing rest-fs

--- index.js
+++ index.js
@@ -1,54 +1,55 @@
 // Create terminal stream for a container using nsenter
+config = {};
 config.fsRoot = "/docker/execdriver/native";
 
 var which = require('which');
 var pty = require('pty.js');
 var nsenter = "";
+var restfs = require('rest-fs');
+
+// var krain = function(Opts, Cb) {
+//   var opts = Opts;
+//   var cb = Cb;
+//   if (typeof opts == 'function') {
+//     cb = Opts;
+//     opts = {};
+//   }
+//   which('nsenter', function(err, cmdpath) {
+//     if (err) {
+//       cb(new Error('nsenter not avalible. module useless'));
+//       return;
+//     }
+//     nsenter = cmdpath;
+//     cb();
+//   });
+// };
+
+//   pid of container to connect to *REQUIRED
+//   Args are argument for nsenter. defaults to all
+//   Opts are option for pty default empty
+//   returns stream to STDIN of container
+
+// var connect = function(pid, Args, Opts) {
+//   var args = Args || "--mount --uts --ipc --net --pid";
+//   var term = pty.spawn('bash',
+//     ["-c", "sudo "+nsenter+" --target " + pid + " " + args],
+//     Opts
+//    );
+//   return term;
+// };
 
 
-var krain = function(Opts, Cb) {
-  var opts = Opts;
-  var cb = Cb;
-  if (typeof opts == 'function') {
-    cb = Opts;
-    opts = {};
-  }
-  which('nsenter', function(err, cmdpath) {
-    if (err) {
-      cb(new Error('nsenter not avalible. module useless'));
-      return;
-    }
-    nsenter = cmdpath;
-    cb();
-  });
-};
-/*
-  pid of container to connect to *REQUIRED
-  Args are argument for nsenter. defaults to all
-  Opts are option for pty default empty
-  returns stream to STDIN of container
-*/
-var connect = function(pid, Args, Opts) {
-  var args = Args || "--mount --uts --ipc --net --pid";
-  var term = pty.spawn('bash',
-    ["-c", "sudo "+nsenter+" --target " + pid + " " + args],
-    Opts
-   );
-  return term;
-};
+// /*
+//   get container filesystem obj
+// */
+// var getFs = function(containerId, cb) {
+//   // validate container id
+//   return {
+//     containerRoot: config.fsRoot + containerId
+//   };
+// };
 
 
-/*
-  get container filesystem obj
-*/
-var getFs = function(containerId, cb) {
-  // validate container id
-  return {
-    containerRoot: config.fsRoot + containerId
-  };
-};
-
-
-module.exports.krain = krain;
-module.exports.connect = connect;
+// module.exports.krain = krain;
+// module.exports.connect = connect;
 
