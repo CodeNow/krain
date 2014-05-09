43f9e7ba19ddcb1bea3c2822fa16befe96368eef
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-05-09 12:52:52 -0700

    add test and container middleware

--- index.js
+++ index.js
@@ -1,12 +1,74 @@
 // Create terminal stream for a container using nsenter
 config = {};
-config.fsRoot = "/docker/execdriver/native";
 
+config.fsRoot = "/docker/execdriver/native";
+if(process.env.NODE_ENV === "test") {
+  config.fsRoot = __dirname+'/test';
+}
+var url = require('url');
+var express = require('express');
+var app = express();
+var path = require('path');
 var which = require('which');
 var pty = require('pty.js');
 var nsenter = "";
 var restfs = require('rest-fs');
+var port = 3000;
+var bodyParser = require('body-parser');
 
+// ensure container is passed correctly
+var containerValidator = function(req, res, next) {
+  if(!(req.body && req.body.container && req.body.container.root)) {
+    return res.send(403);
+  }
+  return next();
+};
+
+var fileMapper = function(req, res, next) {
+  // save trailing slash here to add
+  var oldSlash = "";
+  var newSlash = "";
+
+  // map main url 
+  var dirPath =  decodeURI(url.parse(req.url).pathname);
+  if(dirPath.substr(-1) === '/') {
+    oldSlash = '/';
+  }
+
+  dirPath = path.normalize(dirPath);
+  dirPath = path.resolve(dirPath);
+  dirPath = path.join(config.fsRoot, req.body.container.root, dirPath);
+  req.url = dirPath + oldSlash;
+  // map new path if there is one
+  if(req.body.newPath) {
+    if(req.body.newPath.substr(-1) === '/') {
+      newSlash = '/';
+    }
+    req.body.newPath = path.normalize(req.body.newPath);
+    req.body.newPath = path.resolve(req.body.newPath);
+    req.body.newPath = path.join(config.fsRoot, req.body.container.root, req.body.newPath);
+    req.body.newPath = req.body.newPath + newSlash;
+  }
+  return next();
+};
+
+app.use(bodyParser());
+app.use(containerValidator);
+app.use(fileMapper);
+restfs(app);
+app.listen(port);
+
+/*
+  get container filesystem obj
+*/
+var getFs = function(containerId, cb) {
+  // validate container id
+  return {
+    root: config.fsRoot + containerId
+  };
+};
+
+module.exports = app;
 // var krain = function(Opts, Cb) {
 //   var opts = Opts;
 //   var cb = Cb;
@@ -36,17 +98,6 @@ var restfs = require('rest-fs');
 //     Opts
 //    );
 //   return term;
-// };
-
-
-// /*
-//   get container filesystem obj
-// */
-// var getFs = function(containerId, cb) {
-//   // validate container id
-//   return {
-//     containerRoot: config.fsRoot + containerId
-//   };
 // };
 
 
